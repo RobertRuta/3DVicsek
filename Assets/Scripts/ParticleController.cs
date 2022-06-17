240c76b5bd4dea37628bdebb31b6b4013a6945ac
RobertRuta <77187208+RobertRuta@users.noreply.github.com>
2022-06-17 15:13:07 +0200

    trying to implement Bitonicmerge sorter

--- Assets/Scripts/ParticleController.cs
+++ Assets/Scripts/ParticleController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BufferSorter;
 
 public class ParticleController : MonoBehaviour
 {
     #region variables
 
     public ComputeShader ParticleCalculation;
+    public ComputeShader SortShader;
     public Material ParticleMaterial;
     public int numParticles = 500000;
     public float speed = 4.0f;
@@ -17,8 +19,9 @@ public class ParticleController : MonoBehaviour
     private Vector3Int gridDims;
 
     private const int c_groupSize = 128;
-    private int m_updateParticlesKernel;
     private int m_buildGridIndicesKernel;
+    private int m_updateParticlesKernel;
+    
 
     #endregion
 
@@ -44,7 +47,11 @@ public class ParticleController : MonoBehaviour
     private const int c_quadStride = 12;
 
     private ComputeBuffer m_indicesBuffer;
+    private ComputeBuffer m_valueBuffer;
+    private ComputeBuffer m_keyBuffer;
     private Vector2Int[] temp_indices;
+
+    private int numGroups;
 
     #endregion
 
@@ -65,6 +72,8 @@ public class ParticleController : MonoBehaviour
         m_quadPoints = new ComputeBuffer(6, c_quadStride);
             // Create index buffer
         m_indicesBuffer = new ComputeBuffer(numParticles, 32);
+        m_valueBuffer = new ComputeBuffer(numParticles, 16);
+        m_keyBuffer = new ComputeBuffer(numParticles, 16);
 
 
         // Initialise particles in memory and set initial positions and velocities
@@ -85,7 +94,52 @@ public class ParticleController : MonoBehaviour
         ParticleCalculation.SetFloat("radius", radius);
         ParticleCalculation.SetFloat("cellDim", Mathf.CeilToInt(radius));
         ParticleCalculation.SetFloats("box", new[] {box.x, box.y, box.z});
+
+
+        // Testing
+        // Prepare to run GPU code
+        numGroups = Mathf.CeilToInt((float)numParticles / c_groupSize);
+
+        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "particleIDs", m_keyBuffer);
+        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "cellIDs", m_valueBuffer);
+        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "particles", m_particlesBuffer);
+            // Run code
+        ParticleCalculation.Dispatch(m_buildGridIndicesKernel, numGroups, 1, 1);
         
+        int[] temp_values = new int[numParticles];
+        m_valueBuffer.GetData(temp_values);
+        int[] temp_keys = new int[numParticles];
+        m_keyBuffer.GetData(temp_keys);
+        Particle[] temp_particles = new Particle[numParticles];
+        m_particlesBuffer.GetData(temp_particles);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            print("particle " + i + ": " + temp_keys[i] + " | " + temp_values[temp_keys[i]] + "    pos = " + temp_particles[temp_keys[i]].position);
+            //print(temp_values[i]);
+        }
+
+        print("");
+        print("Sorting somehow...");
+        print("");
+
+        /*
+        */
+        using (Sorter sorter = new Sorter(SortShader))
+        {
+            // values and keys have to be separate compute buffers
+            sorter.Sort(m_valueBuffer, m_keyBuffer);
+        }
+
+        m_valueBuffer.GetData(temp_values);
+        m_keyBuffer.GetData(temp_keys);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            print("particle " + i + ": " + temp_keys[i] + " | " + temp_values[temp_keys[i]] + "    pos = " + temp_particles[temp_keys[i]].position);
+            //print(temp_values[i]);
+        }
+
     }
 
     #endregion
@@ -105,8 +159,6 @@ public class ParticleController : MonoBehaviour
         ParticleCalculation.SetFloats("box", new[] {box.x, box.y, box.z});
         ParticleCalculation.SetInts("gridDims", new[] {gridDims.x, gridDims.y, gridDims.z});
 
-        // Prepare to run GPU code
-        int numGroups = Mathf.CeilToInt((float)numParticles / c_groupSize);
 
         // Dispatch particle update code on GPU
             // Update GPU buffer with CPU buffer ?? Other way round?
@@ -116,30 +168,39 @@ public class ParticleController : MonoBehaviour
 
         // Dispatch grid update code on GPU
             // Update GPU buffer with CPU buffer ?? Other way round?
-        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "cellparticleindices", m_indicesBuffer);
+        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "particleIDs", m_keyBuffer);
+        ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "cellIDs", m_valueBuffer);
             // Update GPU buffer with CPU buffer ?? Other way round?
         ParticleCalculation.SetBuffer(m_buildGridIndicesKernel, "particles", m_particlesBuffer);
             // Run code
         ParticleCalculation.Dispatch(m_buildGridIndicesKernel, numGroups, 1, 1);
 
+        /*
+        */
+        using (Sorter sorter = new Sorter(SortShader))
+        {
+            // values and keys have to be separate compute buffers
+            sorter.Sort(m_valueBuffer, m_keyBuffer);
+        }
 
         #region debugging
         
-        temp_indices = new Vector2Int[numParticles];
-        m_indicesBuffer.GetData(temp_indices);
+        int[] temp_values = new int[numParticles];
+        m_valueBuffer.GetData(temp_values);
+        int[] temp_keys = new int[numParticles];
+        m_keyBuffer.GetData(temp_keys);
 
-        int max = temp_indices[0].y;
-
-        for (int i = 0; i < numParticles; i++)
+            /*
+        for (int i = 0; i < 10; i++)
         {
+            print(i + ": " + temp_keys[i] + " | " + temp_values[i]);
             //max = max < temp_indices[i].y ? temp_indices[i].y : max;
             if (max < temp_indices[i].y)
             {
                 max = temp_indices[i].y;
             }
-            
         }
-        print("max: " + max);
+            */
         
         #endregion
 
